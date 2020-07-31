bf211d84897992fc62d5cc228bacc8f49ee173ae
XuHuifang2020 <983319682@qq.com>
2020-07-31 22:42:50 +0800

    node

---
+++ src/Nowcoder/TreeNode.java
@@ -0,0 +1,14 @@
+package Nowcoder;
+
+public class TreeNode //
+{
+    //
+    int val;
+    TreeNode left= null;
+    TreeNode right= null;
+    TreeNode(int r)
+    {
+        //
+        val= r;
+    }
+}
