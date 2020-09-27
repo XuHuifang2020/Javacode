a7db0af817f51bc63c7b4c610d2205180375d02d
XuHuifang2020 <983319682@qq.com>
2020-09-27 18:56:52 +0800

    main

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,23 +1,42 @@
 package Test;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        while(in.hasNext())
-        {
-            int month = in.nextInt();
-            System.out.println(getTotalCount(month));
-        }
+        Integer[] num = {3,5,11,12};
+        System.out.println(Arrays.toString(find_prime(num)));
     }
 
-    public static int getTotalCount(int m)
+    public static Integer[] find_prime(Integer[] num)
     {
-        if(m <= 2) return 1;
-        else return getTotalCount(m - 1) + getTotalCount(m - 2);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0; i < num.length; i++)
+        {
+            if(issushu(num[i])) arr.add(num[i]);
+        }
+        Integer[] res = new Integer[arr.size()];
+        for(int j = 0; j < res.length; j++)
+        {
+            res[j] = arr.get(j);
+        }
+        return res;
+    }
+
+    public static boolean issushu(int m)
+    {
+            if(m == 0) return false;
+            int j = 2;
+            while(m % j != 0)
+            {
+                j++;
+            }
+            if(j == m) return true;
+            return false;
     }
 }
 
